fa11ae3ae4f882a595afc3ba6d0cfb0077c38fe0
bharathpbhat <bharathpbhat@gmail.com>
2013-12-18 21:01:19 -0800

    Added a contact form

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ LearnRails::Application.routes.draw do
   #     resources :products
   #   end
   LearnRails::Application.routes.draw do
+    resources :contacts, only: [:new, :create]
     root to: 'visitors#new'
   end
 end
